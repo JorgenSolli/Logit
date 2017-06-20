a4d436526f38c74d01d0df1b5160f50f58ad75e8
JorgenSolli <jogex123@gmail.com>
2017-06-20 15:10:37 +0200

    Fixed a migration issue

--- database/migrations/2017_02_23_181915_create_routine_table.php
+++ database/migrations/2017_02_23_181915_create_routine_table.php
@@ -17,7 +17,7 @@ class CreateRoutineTable extends Migration
             $table->increments('id');
             $table->integer('user_id');
             $table->string('routine_name');
-            $table->boolean('status');
+            $table->boolean('active')->default(1);
             $table->timestamps();
         });
     }
