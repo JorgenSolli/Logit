5982d7817ced5390392f8a1259a8fe962b0f51c3
Jørgen Solli <jogex123@gmail.com>
2018-02-15 16:05:31 +0100

    Properly prints out 4 routines per row

--- resources/views/workouts/selectWorkout.blade.php
+++ resources/views/workouts/selectWorkout.blade.php
@@ -4,37 +4,43 @@
   <h2>Glad you decided to hit the gym today!<br><small>Please select a routine</small></h2>
   <div id="routines">
     <div class="row">
-      @foreach($routines as $routine)
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="card card-pricing card-raised">
-            <div class="content">
-              <div class="icon icon-rose">
-                @foreach ($topMuscleGroup[$routine['id']] as $key => $value)
-                  @if ($loop->first)
-                    @php $muscleImage = $key; break; @endphp
-                  @endif
-                @endforeach
-                <img class="muscle-icon" src="/images/icons/muscle_groups/white/{{ $muscleImage }}.svg">
-              </div>
-              <h3 class="card-title">
-                {{ $routine['routine_name'] }}
-                @if (session('gymming') == $routine['id'])
-                  ·&nbsp;<span class="fal fa-clock"></span><small> In progress </small>
-                @endif
-              </h3>
-              <p class="card-description">
-                  Last used: {{ $routine['last_used'] }}
-              </p>
-              @if (session('gymming') == $routine['id'])
-                <button data-href="start/{{ $routine['id'] }}" class="startRoutine btn btn-success">Continue</button>
-              @else
-                <button data-href="start/{{ $routine['id'] }}" class="startRoutine btn btn-success">Start</button>
-              @endif
-              <button data-routine-preview="{{ $routine['id'] }}" class="btn btn-primary">Preview</button>
+        @php $index = 0 @endphp
+        @foreach($routines as $routine)
+            @if ($index == 4)
+                </div><div class="row">
+                @php $index = 0 @endphp
+            @endif
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="card card-pricing card-raised">
+                    <div class="content">
+                        <div class="icon icon-rose">
+                            @foreach ($topMuscleGroup[$routine['id']] as $key => $value)
+                                @if ($loop->first)
+                                    @php $muscleImage = $key; break; @endphp
+                                @endif
+                            @endforeach
+                            <img class="muscle-icon" src="/images/icons/muscle_groups/white/{{ $muscleImage }}.svg">
+                        </div>
+                        <h3 class="card-title">
+                            {{ $routine['routine_name'] }}
+                            @if (session('gymming') == $routine['id'])
+                                ·&nbsp;<span class="fal fa-clock"></span><small> In progress </small>
+                            @endif
+                        </h3>
+                        <p class="card-description">
+                            Last used: {{ $routine['last_used'] }}
+                        </p>
+                        @if (session('gymming') == $routine['id'])
+                            <button data-href="start/{{ $routine['id'] }}" class="startRoutine btn btn-success">Continue</button>
+                        @else
+                            <button data-href="start/{{ $routine['id'] }}" class="startRoutine btn btn-success">Start</button>
+                        @endif
+                        <button data-routine-preview="{{ $routine['id'] }}" class="btn btn-primary">Preview</button>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      @endforeach
+            @php $index++ @endphp
+        @endforeach
     </div>
     @if ($nrInactive > 0)
       <div class="alert alert-info alert-with-icon">
