f4ea30163af0ef709c05a312ac82ac57a424573c
Jørgen Solli <jogex123@gmail.com>
2018-02-15 23:21:16 +0100

    Moved a function to stay DRY

--- app/Classes/LogitFunctions.php
+++ app/Classes/LogitFunctions.php
@@ -2,6 +2,11 @@
 
 namespace Logit\Classes;
 
+use DB;
+use Carbon;
+
+use Logit\Workout;
+
 class LogitFunctions {
 
 	/**
@@ -33,13 +38,6 @@ class LogitFunctions {
             return $getMonth;
         }
         else {
-            function is_leap_year($year) {
-                if ((($year % 4) == 0) && ((($year % 100) != 0) || (($year % 400) == 0))) {
-                    return 29;
-                }
-                return 28;
-            }
-
             # Sets up the expected dataformat
             $monthData = collect([
                 'Jan' => [
@@ -48,7 +46,7 @@ class LogitFunctions {
                 ],
                 'Feb' => [
                     'int' => 2,
-                    'days' => is_leap_year($year)
+                    'days' => LogitFunctions::is_leap_year($year)
                 ],
                 'Mar' => [
                     'int' => 3,
@@ -94,6 +92,99 @@ class LogitFunctions {
 
             return $monthData;
         }
+    }
+
+    /**
+     * User choses one exercise and will be able to see the progress in specified timeframe
+     *
+     * @param  string $type specifies year or month
+     * @param  int $year specifies the year
+     * @param  int $month specifies the mont
+     * @param  string $exercise name of exercise to compare
+     * @return \Illuminate\Http\Response
+     */
+    public static function fetchExerciseData ($type, $month, $year, $exercise, $userId)
+    {
+        $result = array(
+            'labels' => [],
+            'series' => [
+                [],
+                []
+            ],
+            'exercise' => null,
+            'success' => true,
+            'max' => 0,
+        );
+        if ($type == "year") {
+            $workouts = Workout::with(['junction' => function($query) use ($exercise, $year, $userId) {
+                    $query->where([
+                        ['exercise_name', 'like', $exercise],
+                        ['workout_junctions.user_id', $userId],
+                        [DB::raw('YEAR(workout_junctions.created_at)'), '=', date($year)],
+                    ]);
+                }])
+            ->get();
+        }
+        else {
+            # Makes sure the month we get from out request is formated correctly
+            $selectedMonth = ucfirst($month);
+            # Sets up the expected dataformat
+            $monthData = LogitFunctions::parseDate($type, $year, $month);
+
+            $workouts = Workout::with(['junction' => function($query) use ($exercise, $year, $monthData, $selectedMonth, $userId) {
+                    $query->where([
+                        ['exercise_name', 'like', $exercise],
+                        [DB::raw('YEAR(workout_junctions.created_at)'), '=', date($year)],
+                        [DB::raw('MONTH(created_at)'), '=', date($monthData[$selectedMonth]['int'])],
+                        ['workout_junctions.user_id', $userId]
+                    ]);
+                }])
+            ->get();
+        }   
+        
+        foreach ($workouts as $workout) {
+            if ($workout->junction) {
+                foreach ($workout->junction as $junction) {
+                    array_push($result['labels'], Carbon\Carbon::parse($junction->created_at)->format('d/m'));
+
+                    $weight = 0;
+                    if ($junction->weight_type === "assisted") {
+                        $weight = -1 * $junction->weight;
+                    } else {
+                        $weight = $junction->weight;
+                    }
+                    array_push($result['series'][0], $weight);
+
+                    array_push($result['series'][1], $junction->reps);
+                }
+            }
+        }
+
+        $max = 0;
+        foreach ($result['series'] as $series) {
+            foreach ($series as $value) {
+                if ($value > $max - 1) {
+                    $max = $value + 10;
+                }  
+            }
+        }
+
+        $result['max'] = $max;
+
+        if (isset($result['series'][0]) || isset($result['series'][1])) {
+            $result['exercise'] = $exercise;
+        }
+        else {
+            $result['success'] = false;
+        }
+        return $result; 
+    }
+
+    public static function is_leap_year ($year) {
+        if ((($year % 4) == 0) && ((($year % 100) != 0) || (($year % 400) == 0))) {
+            return 29;
+        }
+        return 28;
     }
 
     public static function parseMinutes ($minutes, $format = '%02d:%02d') {
