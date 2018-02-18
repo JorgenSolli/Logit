3cdcfe19fa3ae239c318973b151269366e8f2e69
Jørgen Solli <jogex123@gmail.com>
2018-02-18 23:19:17 +0100

    Fixed a bug where the timer-sound would play after the time-target has been reached when playing after pausing. Music on mobile devices will no longer be lowered every time the timer is re-initiated.

--- resources/assets/js/timer.js
+++ resources/assets/js/timer.js
@@ -6,38 +6,68 @@ var timer = new Timer();
 
 if (timerSettings.play_sound) {    
     var ding = new Audio('/media/ding.wav'); 
+    ding.volume = 0;
 }
 
 $(document).ready(function() {
-    var timerHtml = '<div id="timer">' +
-                        '<div class="row">' +
-                            '<div class="col-xs-4 text-center">' +
-                                '<div id="timerValues"></div>' +
+    var loadTimerHtml = '<div id="timer">' +
+                            '<div class="row">' +
+                                '<div class="col-xs-12 text-center">' +
+                                    '<button id="loadTimer" class="btn btn-rose">' +
+                                        '<i class="far fa-clock"></i> Load timer' + 
+                                    '</button>' +
+                                '</div>' +
                             '</div>' +
-                            '<div class="col-xs-4 text-center">' +
-                                '<i id="timer-play" class="fal fa-play"></i>' +
-                            '</div>' +
-                            '<div class="col-xs-4 text-center">' +
-                                '<i id="timer-reset" class="fal fa-repeat"></i>' +
-                            '</div>' +
+                        '</div>';
+
+    var timerHtml = '<div class="row">' +
+                        '<div class="col-xs-4 text-center">' +
+                            '<div id="timerValues"></div>' +
+                        '</div>' +
+                        '<div class="col-xs-4 text-center">' +
+                            '<i id="timer-play" class="fal fa-play"></i>' +
+                        '</div>' +
+                        '<div class="col-xs-4 text-center">' +
+                            '<i id="timer-reset" class="fal fa-repeat"></i>' +
                         '</div>' +
                     '</div>';
 
-    $("#app").append(timerHtml);
+    $("#app").append(loadTimerHtml);
     $("footer.footer").addClass("hasTimer");
 
-    if (timerSettings.direction == 'default') {
-        $("#timerValues").html('00:00');
-    } else {
-        cosmeticSec = timerSettings.seconds < 10 ? cosmeticSec = ('0' + timerSettings.seconds).slice(-2) : cosmeticSec = timerSettings.seconds;
-        cosmeticMin = timerSettings.minutes < 10 ? cosmeticMin = ('0' + timerSettings.minutes).slice(-2) : cosmeticMin = timerSettings.minutes;
-        $("#timerValues").html(cosmeticMin + ':' + cosmeticSec);
+    $(document).on('click', '#loadTimer', function() {
+        $("#timer").html(timerHtml);
+        
+        // Loads the sound so it's initialized on all phone devices. 
+        if (timerSettings.play_sound) {
+            ding.play();
+        }
+
+        if (timerSettings.direction == 'default') {
+            $("#timerValues").html('00:00');
+        } else {
+            cosmeticSec = timerSettings.seconds < 10 ? cosmeticSec = ('0' + timerSettings.seconds).slice(-2) : cosmeticSec = timerSettings.seconds;
+            cosmeticMin = timerSettings.minutes < 10 ? cosmeticMin = ('0' + timerSettings.minutes).slice(-2) : cosmeticMin = timerSettings.minutes;
+            $("#timerValues").html(cosmeticMin + ':' + cosmeticSec);
+        }
+    });
+
+    var playSound = function() {
+        ding.volume = 1.0;
+        ding.play();
+
+        hasPlayedAudio = true;
     }
 
     $(document).on('click', '#timer-play', function() {
         hasPlayedAudio = false;
         
         if (timerSettings.direction == 'default') {
+            var time = timer.getTimeValues();
+            // If the audio has already been played with the current timer-session
+            if (time && time.minutes == timerSettings.minutes && time.seconds >= timerSettings.seconds) {
+                hasPlayedAudio = true;
+            }
             timer.start();
         } else {
             timer.start({
@@ -49,10 +79,6 @@ $(document).ready(function() {
             });
         }
         $(this).removeClass("fa-play").addClass('fa-pause').attr('id', "timer-pause");
-        // Loads the sound imidediately so its initialized on all phone devices. 
-        if (timerSettings.play_sound) {
-            ding.play(); ding.pause();
-        }
     });
 
     $(document).on('click', '#timer-pause', function() {
@@ -69,20 +95,17 @@ $(document).ready(function() {
     timer.addEventListener('secondsUpdated', function (e) {
         $('#timer #timerValues').html(timer.getTimeValues().toString(['minutes', 'seconds']));
         var time = timer.getTimeValues();
-
+        
         if (timerSettings.direction == 'default') {
-            if (time.seconds >= timerSettings.seconds && time.minutes == timerSettings.minutes && timerSettings.play_sound && !hasPlayedAudio) {
-                ding.play();
-                hasPlayedAudio = true;
+            if (time.minutes == timerSettings.minutes && time.seconds >= timerSettings.seconds && timerSettings.play_sound && !hasPlayedAudio) {
+                playSound();
             }
         } else {
-            if (time.minutes == 0 && time.seconds < 1) {
-                ding.play();
-                hasPlayedAudio = true;
+            if (time.minutes == 0 && time.seconds < 1 && timerSettings.play_sound && !hasPlayedAudio) {
+                playSound();
                 $("#timer-pause").removeClass("fa-pause").addClass('fa-play').attr('id', "timer-play");
             }
         }
-
     });
 
     timer.addEventListener('started', function (e) {
